a3327e57b07c42c6ff38554d5440b1f59e9f1bc2
Seongyeon Mun <msy9680@naver.com>
2024-05-24 17:05:40 +0900

    login and register

--- app/src/main/java/com/example/safedelivery/MainActivity.java
+++ app/src/main/java/com/example/safedelivery/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.safedelivery;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -14,8 +18,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    private FirebaseAuth mFirebaseAuth;
+    private Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +30,51 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webview);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
+        // 버튼 초기화
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.naver.com");
+        // 버튼 클릭 이벤트 설정
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 기능 1 실행
+                Toast.makeText(MainActivity.this, "기능 1 실행", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 기능 2 실행
+                Toast.makeText(MainActivity.this, "기능 2 실행", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 기능 3 실행
+                Toast.makeText(MainActivity.this, "기능 3 실행", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 로그아웃 하기
+                mFirebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        // 탈퇴처리 -> mFirebaseAuth.getCurrentUser().delete();
     }
 }
